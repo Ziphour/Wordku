6b0b0aba5efd82cd2c9e93198848b9b02181c524
Ziphour <znpinbox@gmail.com>
2025-09-05 18:39:27 +0100

    Created initiate Gameboard, find letter and set letter panel function

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,54 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [letters, setLetters] = useState([]);
+  const [gameBoard, setGameBoard] = useState([]);
+  const [letter, setLetter] = useState({
+    Letter: "",
+    Coordinates: [],
+  });
+
+  useEffect(() => {
+    initiateGameboard();
+  }, []);
+
+  const initiateGameboard = () => {
+    for (let i = 0; i < 3; i++) {
+      for (let j = 0; j < 3; j++) {
+        const begginingOfBoard = gameBoard.slice(i, j + 1);
+        const endOfBoard = gameBoard.slice(j);
+        setGameBoard([...begginingOfBoard, [letter], ...endOfBoard]);
+      }
+    }
+  };
+
+  const findLetter = () => {
+    // html coordinates gotten
+    return gameBoard[(i, j)].Letter;
+  };
+
+  const setLetterPanel = () => {
+    // Find co-ordinates
+    // How does the letter clicked relate to, ah, can use co-ord's
+    const letterPanel = findLetter();
+
+    const i = letterPanel.coordinates[0];
+    const j = letterPanel.coordinates[1];
+
+    const letter = letterPanel.Letter;
+    // Inputs letter into J and I
+    setGameBoard((gameBoard) => {
+      const begginingOfBoard = gameBoard.slice(i, j + 1);
+      const endOfBoard = gameBoard.slice(j)(
+        ...begginingOfBoard,
+        [letter],
+        ...endOfBoard
+      );
+    });
+  };
 
   return (
     <>
@@ -29,7 +73,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
