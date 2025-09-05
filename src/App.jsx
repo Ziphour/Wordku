368f52073b291e9c74f00826f918b736a2360c2a
Ziphour <znpinbox@gmail.com>
2025-09-05 19:54:56 +0100

    Updated board logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
 import { useState, useEffect } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+
 import "./App.css";
 
 function App() {
-  const [gameBoard, setGameBoard] = useState([]);
+  const [gameBoard, setGameBoard] = useState();
   const [letter, setLetter] = useState({
     Letter: "",
     Coordinates: [],
@@ -18,31 +17,31 @@ function App() {
     for (let i = 0; i < 3; i++) {
       for (let j = 0; j < 3; j++) {
         setLetter((prev) => ({ ...prev, Coordinates: [i, j] }));
-        const begginingOfBoard = gameBoard.slice(i, j + 1);
-        const endOfBoard = gameBoard.slice(j);
-        setGameBoard([...begginingOfBoard, [letter], ...endOfBoard]);
+        setGameBoard((prev) => {
+          [...prev, [letter]];
+        });
       }
     }
   };
 
   const findLetter = () => {
     // html coordinates gotten
+
     return gameBoard[(i, j)];
   };
 
-  const setLetterPanel = () => {
+  const setLetterPanel = (letterPanel) => {
     // Find co-ordinates
     // How do we know the INPUT letter
-    // *
-    const letterPanel = findLetter();
+    // * You don't need the old one, you can just overwrite with the new one
     const i = letterPanel.coordinates[0];
     const j = letterPanel.coordinates[1];
-    // !
-    const letterFromPanel = Letter.pannel;
+    const letterFromPanel = letterPanel.Letter;
     setLetter({ Letter: letterFromPanel, Coordinates: [i, j] });
 
     const letter = letterPanel.Letter;
     // Inputs letter into J and I
+    //? Will this not insert instead of changing it
     setGameBoard((gameBoard) => {
       const begginingOfBoard = gameBoard.slice(i, j + 1);
       const endOfBoard = gameBoard.slice(j)(
@@ -58,7 +57,15 @@ function App() {
       {/* For loop with co-ords in there */}
       {gameBoard.map((panel) => {
         <div key={panel.coordinates} Coordinates={panel.coordinates}>
-          {panel.Letter}
+          <input
+            type="text"
+            name=""
+            id=""
+            maxLength={1}
+            value={panel.Letter}
+            onChange={(e) => setLetterPanel(e.target.value)}
+          />
+          {panel.letter}
         </div>;
       })}
     </>
